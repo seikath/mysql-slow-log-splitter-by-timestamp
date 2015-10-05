6015b326a220ca1c0df69b18ff4bcda53eb580c7
seikath <gmail@gmaiul.com>
2015-10-05 18:27:21 +0200

    more fixes for the error log name

--- make.weekly.index.usage.report.sh
+++ make.weekly.index.usage.report.sh
@@ -7,19 +7,28 @@ echo "$(date) : checking if the ${0} is executable} .. "
 test ! -x ${0} && echo "$(date) : ${0} is non executable. Please chmod +x it and try again" && exit 0 
 echo "$(date) : the ${0} is executable} .. "
 
+# loading the slow log filename as argument 
+test -z ${@} & echo "$(date) : loading the slow log as an argument failed, please  use the scritp as ${0} full_file_name!"
+_slow_log=${1}
+
+# disabled for the v2.x
 # include the user / host / password for future usage with pt-index-usage
-echo "$(date) : checking the ${0%.*}.conf .."
-test ! -f ${0%.*}.conf && echo "$(date) : the config file ${0%.*}.conf file is missing, exiting noe" && exit 0
-echo "$(date) : loading the ${0%.*}.conf .."
-source ${0%.*}.conf
-test $?0 -gt 0 && echo "$(date) : config file failed to load" && exit 0
-test -z $_user && echo "$(date) : loading the ${0%.*}.conf loaded but there is missing _user value. cannot continue, exiting" && exit 0
+##################################################################################################################################################
+###
+### echo "$(date) : checking the ${0%.*}.conf .."
+### test ! -f ${0%.*}.conf && echo "$(date) : the config file ${0%.*}.conf file is missing, exiting noe" && exit 0
+### echo "$(date) : loading the ${0%.*}.conf .."
+### source ${0%.*}.conf
+### test $?0 -gt 0 && echo "$(date) : config file failed to load" && exit 0
+### test -z $_user && echo "$(date) : loading the ${0%.*}.conf loaded but there is missing _user value. cannot continue, exiting" && exit 0
+### 
+##################################################################################################################################################
 
 # better solution as we might execute it any day beore the next friday .. 
 get_start_friday_unix_timestamp=$(date -d "last friday" +%s)
 get_end_friday_unix_timestamp=$(date -d "last saturday" +%s)
 
-slow_log=${$_slow_log}
+slow_log=${_slow_log}
 # check the file is readable 
 test ! -f ${slow_log} && echo "the slow log ${slow_log} is non existing" && exit 0 
 echo "$(date) : proceeding with the yesterday part of the slow log ..."
